efe902b169551e3bff1443792aad40ee3394e4c7
Christian Clavet <clavetc@irrrpgbuilder.sourceforge.net>
2011-01-15 01:26:06 +0000

    - Updated the scripts
    git-svn-id: http://svn.code.sf.net/p/irrrpgbuilder/code@96 528fbfa9-60f6-4f4a-a82b-d133dc65ca33

--- media/scripts/basicFunctions.lua
+++ media/scripts/basicFunctions.lua
@@ -54,13 +54,13 @@ end
 -- far -> farest distance to chasse, no chase if distanceFrom is more than near value
 function chaseObject(name, speed, near, far)
   local x,y,z = getObjectPosition(name)
-  if(speed == nil) then speed = 0.01 end
+  if(speed == nil) then speed = 0.8 end
   y = 0;
   --lookToObject(name)
   lookAt(x,y,z)
     
-  if(far == nil) then far = 3 end
-  if(near == nil) then near = 0.5 end
+  if(far == nil) then far = 600 end
+  if(near == nil) then near = 36 end
   
   if( (distanceFrom(x,y,z) < far) and (distanceFrom(x,y,z) > near) ) then
     move(speed)
--- media/scripts/generic_enemy.lua
+++ media/scripts/generic_enemy.lua
@@ -1,7 +1,7 @@
 local a = 0
 
 function hitplayer()
-  decreasePlayerLife(1)
+  decreasePlayerLife(math.random(1,4))
   a = 0
 end
 
@@ -9,17 +9,20 @@ function step()
   life = getObjectLife(objName);
   if (life == 0) then setEnabled(false) end
   local x,y,z = getObjectPosition("player")
-  if(distanceFrom(x,y,z) < 96) then 
+  if(distanceFrom(x,y,z) < 144) then 
     setObjectLabel("Enemy life:"..life.."/50")
     showObjectLabel()
-    if(distanceFrom(x,y,z) < 32) then
-		chaseObject("player",0.8,32,600)
+    if(distanceFrom(x,y,z) < 60) then
+		--chaseObject("player",0.8,54,600)
+		setAnimation("idle")
+		setFrameLoop(0,0);
+		--move(0);
         if (a==0)then
           programAction(0.5, hitplayer)
           a = 1
         end  
       else
-        chaseObject("player",0.8,32,600)
+        chaseObject("player",0.8,60,600)
       end
   else
     hideObjectLabel()
