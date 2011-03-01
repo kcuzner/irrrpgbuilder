391c4dc637b7f91c314fb3afe1cdaa2118754487
Christian Clavet <clavetc@irrrpgbuilder.sourceforge.net>
2011-03-01 20:00:42 +0000

    -Modified the ennemy script (temporary), until the collision system will not prevent the attacks
    git-svn-id: http://svn.code.sf.net/p/irrrpgbuilder/code@154 528fbfa9-60f6-4f4a-a82b-d133dc65ca33

--- media/scripts/generic_enemy.lua
+++ media/scripts/generic_enemy.lua
@@ -1,7 +1,7 @@
 local a = 0
 
 function setProperties()
--- Using the new properties attributes, can set the NPC combat attributes
+-- Using the new properties attributes, can set the NPC combat attributes. 
 	setEnemy() -- this is an ennemy
 	setPropertie("life",50) -- current life of the NPC
 	setPropertie("maxlife",50) -- current Maximum life of this NPC
@@ -16,6 +16,11 @@ function hitplayer()
   a = 0
 end
 
+function unblock()
+	print("Lua character had a collision, unblocking now")
+	blocked=false
+end
+
 -- "onClicked" will trigger is the dynamic object is being clicked on by the user
 function onClicked()
 	print("Lua character is being clicked on!")
@@ -24,8 +29,8 @@ end
 -- new game function
 -- "onCollision" will trigger if the dynamic object got a collision with something
 function onCollision()
-	print("Lua character had a collision");
-	blocked=true;
+	blocked=true
+	programAction(3, unblock)
 end
 
 -- "step" will trigger at each time interval (around 1/4 second)
@@ -33,18 +38,13 @@ function onUpdate()
   name = getName()..": "..getPropertie("life").."/"..getPropertie("maxlife")
   if (getPropertie("life") == 0) then setEnabled(false) end
   local x,y,z = getObjectPosition("player")
-  if(distanceFrom(x,y,z) < 288) then 
+  if(distanceFrom(x,y,z) < 288 or not blocked) then 
     setObjectLabel(name)
     showObjectLabel()
-    if(distanceFrom(x,y,z) < 66 or blocked) then
+    if(distanceFrom(x,y,z) < 66) then
 		setAnimation("idle")
 		setFrameLoop(0,0);
-		-- reset the collision
-		if (blocked) then
-			blocked=false;
-			a=1
-		end
-		  if (a==0)then
+		if (a==0)then
 		  -- wait 0.5 second then do the attack (the animation is not present for the attack)
           programAction(0.5, hitplayer)
           a = 1 -- this will "block the next attacks until it has been really done"
@@ -60,8 +60,6 @@ end
 
 -- "onLoad" will trigger when the dynamic object is initialized (only once)
 function onLoad()
-	setProperties();
-	blocked = false;
- end
-
-
+	setProperties()
+	blocked = false
+end
