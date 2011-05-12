e55a5904f4094afa43170cf082e75f36e4f2264a
Christian Clavet <clavetc@irrrpgbuilder.sourceforge.net>
2011-05-12 02:05:17 +0000

    - Added Hurt-Die-Despawn for the goblin in a single anim (make sense) - Adding the script I forgot to include to have the goblin trigger the attack animation. - Will have to check the animation and combat system to applie thoses new animations. (Controlled by the game)
    git-svn-id: http://svn.code.sf.net/p/irrrpgbuilder/code@206 528fbfa9-60f6-4f4a-a82b-d133dc65ca33

---
+++ media/scripts/AI_goblin.lua
@@ -0,0 +1,57 @@
+local a = 0
+
+function setProperties()
+-- Using the new properties attributes, can set the NPC combat attributes. 
+	setEnemy() -- this is an ennemy
+	setPropertie("life",50) -- current life of the NPC
+	setPropertie("maxlife",50) -- current Maximum life of this NPC
+	setPropertie("experience",10) -- given experience to attacker if killed
+	setPropertie("mindamage",2) -- minimum damage inflicted
+	setPropertie("maxdamage",5) -- maximum damage inflicted
+	setPropertie("hurtresist",50) -- resistance to attacks in %. If obtained, trigger the "hurt" animation then return to idle, breaking the current attack
+end
+
+function hitplayer()
+  attack("player")
+  a = 0
+end
+
+-- "onClicked" will trigger is the dynamic object is being clicked on by the user
+function onClicked()
+	print("Lua character is being clicked on!")
+end
+
+-- new game function
+-- "onCollision" will trigger if the dynamic object got a collision with something
+function onCollision()
+end
+
+-- "step" will trigger at each time interval (around 1/4 second)
+function onUpdate()
+  name = getName()..": "..getPropertie("life").."/"..getPropertie("maxlife")
+  if (getPropertie("life") == 0) then setEnabled(false) end
+  local x,y,z = getObjectPosition("player")
+  if(distanceFrom(x,y,z) < 288) then 
+    setObjectLabel(name)
+    showObjectLabel()
+    if(distanceFrom(x,y,z) < 66) then
+		setAnimation("attack")
+		if (a==0)then
+		  -- wait 0.5 second then do the attack (the animation is not present for the attack)
+          programAction(0.5, hitplayer)
+          a = 1 -- this will "block the next attacks until it has been really done"
+        end  
+      else
+        chaseObject("player",0.8,66,800)
+      end
+  else
+    hideObjectLabel()
+    walkRandomly()
+  end
+end
+
+-- "onLoad" will trigger when the dynamic object is initialized (only once)
+function onLoad()
+	setProperties()
+	blocked = false
+end
