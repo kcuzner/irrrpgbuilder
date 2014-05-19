34d4c6dbe64b430dbd10e653972740858622a5e4
Christian Clavet <clavetc@irrrpgbuilder.sourceforge.net>
2014-05-19 12:36:28 +0000

    fixed typo in a lua script
    git-svn-id: http://svn.code.sf.net/p/irrrpgbuilder/code@519 528fbfa9-60f6-4f4a-a82b-d133dc65ca33

--- media/scripts/colors.lua
+++ media/scripts/colors.lua
@@ -4,7 +4,7 @@ BLACK = {r = 0,g = 0,b = 0}
 WHITE = {r = 255, g = 255, b = 255} 
 
 RED = {r = 255,g = 0, b = 0} 
-GREE = {r = 0,g = 255,b = 0} 
+GREEN = {r = 0,g = 255,b = 0} 
 BLUE = {r = 0,g = 0,b = 255} 
 
 LIGHT_RED = {r = 255,g = 125,b = 125} 
