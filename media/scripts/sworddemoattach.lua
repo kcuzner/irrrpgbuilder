e7b193a153262b6b03444165a16042fd96a72597
Christian Clavet <clavetc@irrrpgbuilder.sourceforge.net>
2014-12-28 01:33:29 +0000

    + Done the example script for the sword loot item. Will attach it to the character head bone. + Done the props that will be used for the health potions. Correct transparency was tricky.
    git-svn-id: http://svn.code.sf.net/p/irrrpgbuilder/code@627 528fbfa9-60f6-4f4a-a82b-d133dc65ca33

---
+++ media/scripts/sworddemoattach.lua
@@ -0,0 +1,48 @@
+-- Attach demo script
+
+-- will attach the item to the head of the player.
+
+-- The player must hadve the "head" attachment point defined in the XML
+
+
+
+
+-- By Christian Clavet
+
+
+
+
+
+function onUse()
+
+    attachObject("player","head")
+
+end
+
+
+-- Called when the user click on the object or use the interaction button in proximity
+
+function onClicked()
+
+	-- When this object is clicked it will go inside the player backpack
+
+	moveObjectLoot()
+
+end
+
+
+
+
+-- Called when the game start
+
+function onLoad()
+	
+-- Set the display name and show it
+
+	setObjectLabel("Sword")
+
+	showObjectLabel()
+
+	destroyAfterUse(false)
+
+end
